7b541b4e61c5d371940b83d8e637765d18f1b418
Krish Mittal <krishmittal212005@gmail.com>
2024-05-20 15:20:23 +0530

    Initial Commit

---
+++ TKTodoApp/TKTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKTodoAppApp.swift
+//  TKTodoApp
+//
+//  Created by Krish Mittal on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TKTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
